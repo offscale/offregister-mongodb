edc1e049635a542669ec3e4cf42bc80c1af9786e
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-25 22:06:30 -0400

    [*.py] Update to threadsafe Python 3 compatible Fabric ; [README.md] Update support Python version range ; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_mongodb/ubuntu/__init__.py
+++ offregister_mongodb/ubuntu/__init__.py
@@ -1,30 +1,29 @@
-from fabric.contrib.files import append
-from fabric.operations import run, sudo
 from offregister_fab_utils.apt import apt_depends
 from offregister_fab_utils.ubuntu.systemd import restart_systemd
+from patchwork.files import append
 
 
 def install0(**kwargs):
-    installed = lambda: run(
-        "dpkg-query --showformat='${Version}' --show mongodb-org", quiet=True
+    installed = lambda: c.run(
+        "dpkg-query --showformat='${Version}' --show mongodb-org", hide=True
     )
 
     def mongodb_apt_init():
         if kwargs["VERSION"] == "3.4":
-            sudo(
+            c.sudo(
                 "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6"
             )
-            sudo("rm /etc/apt/sources.list.d/mongodb-org*")
+            c.sudo("rm /etc/apt/sources.list.d/mongodb-org*")
             append(
                 "/etc/apt/sources.list.d/mongodb-org-3.4.list",
                 "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse",
                 use_sudo=True,
             )
         elif kwargs["VERSION"] == "3.6":
-            sudo(
+            c.sudo(
                 "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5"
             )
-            sudo("rm /etc/apt/sources.list.d/mongodb-org*")
+            c.sudo("rm /etc/apt/sources.list.d/mongodb-org*")
             append(
                 "/etc/apt/sources.list.d/mongodb-org-3.6.list",
                 "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse",
@@ -37,16 +36,16 @@ def install0(**kwargs):
                 )
             )
 
-    if sudo("dpkg -s mongodb-org", quiet=True, warn_only=True).failed:
+    if c.sudo("dpkg -s mongodb-org", hide=True, warn=True).exited != 0:
         mongodb_apt_init()
-        apt_depends("mongodb-org")
+        apt_depends(c, "mongodb-org")
         restart_systemd("mongod")
         return "MongoDB {} installed".format(installed())
 
     version = installed()
     if not version.startswith(kwargs["VERSION"]):
         mongodb_apt_init()
-        apt_depends("mongodb-org")
+        apt_depends(c, "mongodb-org")
         restart_systemd("mongod")
         return "MongoDB {} installed".format(installed())
 
